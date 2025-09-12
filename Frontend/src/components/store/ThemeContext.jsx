2470355175a0bf133c9916f48c5dda1c73d967c3
Faizann333 <mrfaizanmughal33@gmail.com>
2025-09-12 19:40:44 +0500

    solve problem of darkmode not working on mobile after deployment

--- Frontend/src/components/store/ThemeContext.jsx
+++ Frontend/src/components/store/ThemeContext.jsx
@@ -5,19 +5,19 @@ export const ThemeContext = createContext({
 })
 
 const ThemeProvider = ({children}) => {
-      const [darkMode, setDarkMode] = useState(() => {
+     const [darkMode, setDarkMode] = useState(() => {
+    if (typeof window !== "undefined") {
       const savedTheme = localStorage.getItem("theme");
-  if (savedTheme) {
-    return savedTheme === "dark";
-  }
-  //  default to dark
-  return true;
-});
+      if (savedTheme) return savedTheme === "dark";
+    }
+    return true; // default = dark
+  });
 
 
   useEffect(() => {
     localStorage.setItem("theme", darkMode ? "dark" : "light");
   }, [darkMode]);
+
   return (
     <ThemeContext.Provider value={{darkMode,setDarkMode}}>
         {children}
